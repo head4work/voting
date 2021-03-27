8fff40b1834451c83a51d5544158c74693f61547
apprenticefarter <63342380+apprenticefarter@users.noreply.github.com>
2021-03-27 16:35:16 +0300

    created field added to Restaurant

--- src/main/java/ru/homeproject/voting/model/Restaurant.java
+++ src/main/java/ru/homeproject/voting/model/Restaurant.java
@@ -1,21 +1,26 @@
 package ru.homeproject.voting.model;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 public class Restaurant extends AbstractNamedEntity{
+    private Date created;
+
     private List<Dish> menu;
 
     public Restaurant() {
     }
 
-    public Restaurant(Integer id, String name, List<Dish> menu) {
+    public Restaurant(Integer id, String name, Date created, List<Dish> menu) {
         super(id, name);
+        this.created = created;
         this.menu = menu;
     }
 
-    public Restaurant(Integer id, String name, Dish ... dishes) {
+    public Restaurant(Integer id, String name, Date created, Dish ... dishes) {
         super(id, name);
+        this.created = created;
         this.menu = Arrays.asList(dishes);
     }
 }
--- src/main/java/ru/homeproject/voting/model/User.java
+++ src/main/java/ru/homeproject/voting/model/User.java
@@ -4,11 +4,15 @@ import java.util.Date;
 import java.util.Set;
 
 public class User {
-private String email;
-private String password;
-private  boolean enabled = true;
-private Date registered = new Date();
-private Set<Role> roles;
+    private String email;
+
+    private String password;
+
+    private boolean enabled = true;
+
+    private Date registered = new Date();
+
+    private Set<Role> roles;
 
     public User() {
     }
