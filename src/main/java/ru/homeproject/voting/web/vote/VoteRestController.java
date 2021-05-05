c960570a7bd0e6dff61c25413becd2f592ec421c
apprenticefarter <63342380+apprenticefarter@users.noreply.github.com>
2021-05-05 11:48:32 +0300

    VoteRestControllerTest fix

--- src/main/java/ru/homeproject/voting/web/vote/VoteRestController.java
+++ src/main/java/ru/homeproject/voting/web/vote/VoteRestController.java
@@ -1,12 +1,11 @@
 package ru.homeproject.voting.web.vote;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.homeproject.voting.repository.VoteRepository;
 
 @RestController
-@RequestMapping(value = VoteRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = VoteRestController.REST_URL)
 public class VoteRestController extends AbstractVoteController {
     protected static final String REST_URL = "/rest/vote";
 
