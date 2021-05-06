43ebbc6e2b6674243c538d6ffbdf0f8b49d53dbc
apprenticefarter <63342380+apprenticefarter@users.noreply.github.com>
2021-05-06 12:16:20 +0300

    Spring security added

--- src/test/java/ru/homeproject/voting/web/vote/VoteRestControllerTest.java
+++ src/test/java/ru/homeproject/voting/web/vote/VoteRestControllerTest.java
@@ -1,19 +1,27 @@
 package ru.homeproject.voting.web.vote;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.homeproject.voting.RestaurantTestData;
 import ru.homeproject.voting.web.AbstractRestControllerTest;
+import ru.homeproject.voting.web.restaurant.AbstractRestaurantController;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.homeproject.voting.RestaurantTestData.REST1_ID;
 
 class VoteRestControllerTest extends AbstractRestControllerTest {
 
     private static final String REST_URL = VoteRestController.REST_URL;
 
+    @Autowired
+    private AbstractRestaurantController controller;
+
     @Test
     void createVote() throws Exception {
         perform(MockMvcRequestBuilders.post(REST_URL).param("id", String.valueOf(RestaurantTestData.REST1_ID)))
                 .andExpect(status().isOk());
+        assertEquals(2, controller.countVotes(REST1_ID));
     }
 }
