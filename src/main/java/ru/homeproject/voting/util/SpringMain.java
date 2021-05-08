38db2c59bc03df398d1fd8d3c7f460a6e84de01f
apprenticefarter <63342380+apprenticefarter@users.noreply.github.com>
2021-05-08 12:58:23 +0300

    cleaned commented code

--- src/main/java/ru/homeproject/voting/util/SpringMain.java
+++ src/main/java/ru/homeproject/voting/util/SpringMain.java
@@ -51,21 +51,4 @@ public class SpringMain {
     }
 
   }
-
-
-/* try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
-        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-        AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-        adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
-        System.out.println();
-
-        MealRestController mealController = appCtx.getBean(MealRestController.class);
-        List<MealTo> filteredMealsWithExcess =
-                mealController.getBetween(
-                        LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
-                        LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
-        filteredMealsWithExcess.forEach(System.out::println);
-        System.out.println();
-        System.out.println(mealController.getBetween(null, null, null, null));
-    }*/
 }
